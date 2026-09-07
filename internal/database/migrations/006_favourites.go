9c8513ce867e4f95df78230181aa7985ef79930a
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-07 13:25:05 +0300

    fix(migrations): use MongoDB v2 unique index builder

--- internal/database/migrations/006_favourites.go
+++ internal/database/migrations/006_favourites.go
@@ -1,4 +1,19 @@
 package migrations
-import("context";"go.mongodb.org/mongo-driver/v2/bson";"go.mongodb.org/mongo-driver/v2/mongo";"go.mongodb.org/mongo-driver/v2/mongo/options")
-type Favourites struct{};func(Favourites)Version()int{return 6};func(Favourites)Name()string{return "favourite_riders"}
-func(Favourites)Up(ctx context.Context,db *mongo.Database)error{_,e:=db.Collection("favourite_riders").Indexes().CreateOne(ctx,mongo.IndexModel{Keys:bson.D{{Key:"senderId",Value:1},{Key:"riderId",Value:1}},Options: options.Index()Unique:true});return e}
+
+import (
+ "context"
+ "go.mongodb.org/mongo-driver/v2/bson"
+ "go.mongodb.org/mongo-driver/v2/mongo"
+ "go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type Favourites struct{}
+func(Favourites)Version()int{return 6}
+func(Favourites)Name()string{return "favourite_riders"}
+func(Favourites)Up(ctx context.Context,db *mongo.Database)error{
+ _,e:=db.Collection("favourite_riders").Indexes().CreateOne(ctx,mongo.IndexModel{
+  Keys:bson.D{{Key:"senderId",Value:1},{Key:"riderId",Value:1}},
+  Options:options.Index().SetUnique(true),
+ })
+ return e
+}
