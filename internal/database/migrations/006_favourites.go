ec3a102e652d08657aa990eaa418c63270fd8319
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-07 13:16:28 +0300

    fix(migrations): use MongoDB v2 index options API

--- internal/database/migrations/006_favourites.go
+++ internal/database/migrations/006_favourites.go
@@ -1,4 +1,4 @@
 package migrations
-import("context";"go.mongodb.org/mongo-driver/v2/bson";"go.mongodb.org/mongo-driver/v2/mongo")
+import("context";"go.mongodb.org/mongo-driver/v2/bson";"go.mongodb.org/mongo-driver/v2/mongo";"go.mongodb.org/mongo-driver/v2/mongo/options")
 type Favourites struct{};func(Favourites)Version()int{return 6};func(Favourites)Name()string{return "favourite_riders"}
-func(Favourites)Up(ctx context.Context,db *mongo.Database)error{_,e:=db.Collection("favourite_riders").Indexes().CreateOne(ctx,mongo.IndexModel{Keys:bson.D{{Key:"senderId",Value:1},{Key:"riderId",Value:1}},Options:&mongo.IndexOptions{Unique:true}});return e}
+func(Favourites)Up(ctx context.Context,db *mongo.Database)error{_,e:=db.Collection("favourite_riders").Indexes().CreateOne(ctx,mongo.IndexModel{Keys:bson.D{{Key:"senderId",Value:1},{Key:"riderId",Value:1}},Options:options.Index(){Unique:true}});return e}
